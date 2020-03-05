14d4c4d3a1e60f1cbb3250d5a65c25bfea49cf81
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-03-05 10:36:53 -0800

    wrap at 15 characters

--- jtbl/cli.py
+++ jtbl/cli.py
@@ -11,4 +11,10 @@ if sys.stdin.isatty():
 
 data = json.loads(sys.stdin.read())
 
+# wrap every 15 chars for all field values
+for entry in data:
+    for k, v in entry.items():
+        if v is not None:
+            entry[k] = '\n'.join([str(v)[i:i + 15] for i in range(0, len(str(v)), 15)])
+
 print(tabulate.tabulate(data, headers='keys'))
