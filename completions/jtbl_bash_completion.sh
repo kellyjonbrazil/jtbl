1ec08f7816b78f394e8536f8376b0a2a22de2eab
Kelly Brazil <kellyjonbrazil@gmail.com>
2023-12-10 15:48:55 -0800

    use LAST_WORD variable

--- completions/jtbl_bash_completion.sh
+++ completions/jtbl_bash_completion.sh
@@ -2,12 +2,13 @@ _jtbl()
 {
     OPTIONS=(--cols -c --csv -d --dokuwiki -f --fancy -h --help -H --html -m --markdown -n --no-wrap -q --quiet -r --rotate -t --truncate -v --version)
     MOD_OPTIONS=(--cols -n --no-wrap -q --quiet -t --truncate)
+    LAST_WORD=$(echo "${COMP_WORDS[${#COMP_WORDS[@]}-1]}")
 
     if [ "${#COMP_WORDS[@]}" != "2" ]; then
-        COMPREPLY=($(compgen -W "${MOD_OPTIONS[*]}" -- "${COMP_WORDS[${#COMP_WORDS[@]}-1]}"))
+        COMPREPLY=($(compgen -W "${MOD_OPTIONS[*]}" -- "$LAST_WORD"))
         return 0
     else
-        COMPREPLY=($(compgen -W "${OPTIONS[*]}" -- "${COMP_WORDS[${#COMP_WORDS[@]}-1]}"))
+        COMPREPLY=($(compgen -W "${OPTIONS[*]}" -- "$LAST_WORD"))
         return 0
     fi
 } &&
